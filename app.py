44d5c39b83f63454d159d31c2698644f15c4f0c8
Gyanesh Kumar <jeeke7@gmail.com>
2020-03-15 20:19:10 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,116 @@
+# Dependencies
+from flask import Flask, request, jsonify
+import traceback
+import json
+import requests
+
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+# from sklearn.externals import joblib
+from sklearn.cluster import KMeans
+
+# Your API definition
+app = Flask(__name__)
+global knn
+
+@app.route('/train', methods=['GET'])
+def train():
+    url = "https://falcons-cyber.firebaseio.com/train.json"
+    r = requests.get(url)
+    v = r.json()
+    # print(v)
+
+    trans=pd.DataFrame(v)
+    df=trans.T
+    df.reset_index(inplace=True,drop=True)
+    df.head()
+
+    clus=df[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable']]
+
+    km = KMeans(n_clusters=2,random_state=1)
+    km.fit(clus)
+    y = km.fit_predict(clus)
+    a=pd.DataFrame({'Cyberloafer Type':km.labels_,'Name of the Employee':df['name']})
+    l = sum(a['Cyberloafer Type']==0)
+    h = sum(a['Cyberloafer Type']==1)
+    d = {}
+    for i in range(0,len(a)):
+        d[str(a.iloc[i][1])] = a.iloc[i][0].astype('str')
+    res = {
+        'list' : d,
+        'graph' : {
+            '0' : l,
+            '1' : h
+        }
+    }
+    import json
+    with open(r'model2_static.json', 'w') as f:
+        json.dump(res, f)
+
+    a=pd.DataFrame({'Cyberloafer Type':km.labels_,'Name of the Employee':df['name']})
+
+    clus['Cyberloafer Type']=a['Cyberloafer Type']
+
+    a.set_index('Name of the Employee',inplace=True)
+    # a['Cyberloafer Type']=a['Cyberloafer Type'].map({0:'High Cyberloafer',1:'Low Cyberloafer'})
+    # a.head()
+    X=clus[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable']]
+    y=clus['Cyberloafer Type']
+    global knn
+    knn = KNeighborsClassifier()
+    knn.fit(X,y)
+    return ('Model Trained')
+    # joblib.dump(knn, 'model3.pkl')
+    # print("Model3 dumped!")
+
+
+@app.route('/prediction', methods=['GET'])
+def prediction():
+    # knn = joblib.load("model3.pkl")# Load "model.pkl"
+    # print ('knn loaded')
+    if knn:
+        try:
+            url = "https://falcons-cyber.firebaseio.com/train.json"
+            m = requests.get(url)
+            n = m.json()
+            o=pd.DataFrame(n)
+            p =o.T
+            p.reset_index(inplace=True,drop=True)
+            q=p[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable','name']]
+            q.set_index('name',inplace=True)
+            knn_pred=knn.predict(q)
+            out=pd.DataFrame({'Cyberloafer Type':knn_pred,'Name':q.index})
+            out.set_index('Name',inplace=True)
+            out.reset_index(inplace=True)
+            print(out)
+            d1 = []
+            for i in range(0,len(out)):
+                dic = { 'name' : out.iloc[i][0],
+                        'prediction' : out.iloc[i][1].astype('str') }
+                d1.append(dic) 
+            return json.dumps(d1)
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        return ('Train the model first')
+
+
+@app.route('/canalysis', methods=['GET'])
+def canalysis():
+        try:
+            with open(r'model2_static.json') as f:
+                return json.load(f)
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1]) # This is for a command-line input
+    except:
+        port = 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
