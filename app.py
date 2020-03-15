254a3aac50904e9ec34dce61ee6f3c49a47797d7
Gyanesh Kumar <jeeke7@gmail.com>
2020-03-16 00:01:41 +0530

    6

--- app.py
+++ app.py
@@ -22,56 +22,57 @@ def root():
 
 @app.route('/train', methods=['GET'])
 def train():
-    url = "https://falcons-cyber.firebaseio.com/predict.json"
+    url = "https://falcons-cyber.firebaseio.com/train.json"
     r = requests.get(url)
     if r:
-        return jsonify({'message': "No data here to train"})
-    v = r.json()
-    # print(v)
-    trans=pd.DataFrame(v)
-    df=trans.T
-    df.reset_index(inplace=True,drop=True)
-    df.head()
+        v = r.json()
+        # print(v)
+        trans=pd.DataFrame(v)
+        df=trans.T
+        df.reset_index(inplace=True,drop=True)
+        df.head()
 
-    clus=df[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable']]
+        clus=df[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable']]
 
-    km = KMeans(n_clusters=2,random_state=1)
-    km.fit(clus)
-    y = km.fit_predict(clus)
-    a=pd.DataFrame({'Cyberloafer Type':km.labels_,'Name of the Employee':df['name']})
-    l = sum(a['Cyberloafer Type']==0)
-    h = sum(a['Cyberloafer Type']==1)
-    d = []
-    for i in range(0,len(a)):
-        dic = { 'name' : a.iloc[i][1],
-                        'prediction' : a.iloc[i][0].astype('str') }
-        d.append(dic)
-    res = {
-        'list' : d,
-        'graph' : {
-            'low' : l,
-            'high' : h
+        km = KMeans(n_clusters=2,random_state=1)
+        km.fit(clus)
+        y = km.fit_predict(clus)
+        a=pd.DataFrame({'Cyberloafer Type':km.labels_,'Name of the Employee':df['name']})
+        l = sum(a['Cyberloafer Type']==0)
+        h = sum(a['Cyberloafer Type']==1)
+        d = []
+        for i in range(0,len(a)):
+            dic = { 'name' : a.iloc[i][1],
+                            'prediction' : a.iloc[i][0].astype('str') }
+            d.append(dic)
+        res = {
+            'list' : d,
+            'graph' : {
+                'low' : l,
+                'high' : h
+            }
         }
-    }
-    import json
-    with open(r'model2_static.json', 'w') as f:
-        json.dump(res, f)
+        import json
+        with open(r'model2_static.json', 'w') as f:
+            json.dump(res, f)
 
-    a=pd.DataFrame({'Cyberloafer Type':km.labels_,'Name of the Employee':df['name']})
+        a=pd.DataFrame({'Cyberloafer Type':km.labels_,'Name of the Employee':df['name']})
 
-    clus['Cyberloafer Type']=a['Cyberloafer Type']
+        clus['Cyberloafer Type']=a['Cyberloafer Type']
 
-    a.set_index('Name of the Employee',inplace=True)
-    # a['Cyberloafer Type']=a['Cyberloafer Type'].map({0:'High Cyberloafer',1:'Low Cyberloafer'})
-    # a.head()
-    X=clus[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable']]
-    y=clus['Cyberloafer Type']
-    global knn
-    knn = KNeighborsClassifier()
-    knn.fit(X,y)
-    return jsonify({'message': "Model Trained"})
-    # joblib.dump(knn, 'model3.pkl')
-    # print("Model3 dumped!")
+        a.set_index('Name of the Employee',inplace=True)
+        # a['Cyberloafer Type']=a['Cyberloafer Type'].map({0:'High Cyberloafer',1:'Low Cyberloafer'})
+        # a.head()
+        X=clus[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable']]
+        y=clus['Cyberloafer Type']
+        global knn
+        knn = KNeighborsClassifier()
+        knn.fit(X,y)
+        return jsonify({'message': "Model Trained"})
+        # joblib.dump(knn, 'model3.pkl')
+        # print("Model3 dumped!")
+    else :
+        return jsonify({'message': "No data here to train"})
 
 
 @app.route('/prediction', methods=['GET'])
@@ -80,25 +81,28 @@ def prediction():
     # print ('knn loaded')
     if knn:
         try:
-            url = "https://falcons-cyber.firebaseio.com/train.json"
+            url = "https://falcons-cyber.firebaseio.com/predict.json"
             m = requests.get(url)
-            n = m.json()
-            o=pd.DataFrame(n)
-            p =o.T
-            p.reset_index(inplace=True,drop=True)
-            q=p[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable','name']]
-            q.set_index('name',inplace=True)
-            knn_pred=knn.predict(q)
-            out=pd.DataFrame({'Cyberloafer Type':knn_pred,'Name':q.index})
-            out.set_index('Name',inplace=True)
-            out.reset_index(inplace=True)
-            print(out)
-            d1 = []
-            for i in range(0,len(out)):
-                dic = { 'name' : out.iloc[i][0],
-                        'prediction' : out.iloc[i][1].astype('str') }
-                d1.append(dic) 
-            return json.dumps(d1)
+            if m:
+                n = m.json()
+                o=pd.DataFrame(n)
+                p =o.T
+                p.reset_index(inplace=True,drop=True)
+                q=p[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable','name']]
+                q.set_index('name',inplace=True)
+                knn_pred=knn.predict(q)
+                out=pd.DataFrame({'Cyberloafer Type':knn_pred,'Name':q.index})
+                out.set_index('Name',inplace=True)
+                out.reset_index(inplace=True)
+                print(out)
+                d1 = []
+                for i in range(0,len(out)):
+                    dic = { 'name' : out.iloc[i][0],
+                            'prediction' : out.iloc[i][1].astype('str') }
+                    d1.append(dic) 
+                return json.dumps(d1)
+            else :
+                return jsonify({'message': "No data here to predict"})
         except:
             return jsonify({'trace': traceback.format_exc()})
     else:
