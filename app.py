bde5a068e16bd282e75880cb0158f3415002871f
Gyanesh Kumar <jeeke7@gmail.com>
2020-03-18 22:06:58 +0530

    graph-syntax

--- app.py
+++ app.py
@@ -14,21 +14,24 @@ from sklearn.cluster import KMeans
 
 # Your API definition
 app = Flask(__name__)
-global knn 
+global knn
 knn = None
 global model2FileData
 model2FileData = None
+
 
 @app.route('/', methods=['GET'])
 def root():
     return jsonify({'message': "Welcome to Falcon\'s Web API"})
 
+
 @app.route('/untrain', methods=['GET'])
 def untrain():
-    global knn,model2FileData
+    global knn, model2FileData
     knn = None
     model2FileData = None
     return jsonify({'message': "Model Untrained"})
+
 
 @app.route('/train', methods=['GET'])
 def train():
@@ -37,48 +40,52 @@ def train():
     if r:
         v = r.json()
         # print(v)
-        trans=pd.DataFrame(v)
-        df=trans.T
-        df.reset_index(inplace=True,drop=True)
+        trans = pd.DataFrame(v)
+        df = trans.T
+        df.reset_index(inplace=True, drop=True)
         df.head()
 
-        clus=df[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable']]
+        clus = df[['timeSpentOnInternet',
+                   'peopleAroundUsesInternet', 'internetUseEnjoyable']]
 
-        km = KMeans(n_clusters=2,random_state=1)
+        km = KMeans(n_clusters=2, random_state=1)
         km.fit(clus)
         y = km.fit_predict(clus)
-        a=pd.DataFrame({'Cyberloafer Type':km.labels_,'Name of the Employee':df['name']})
-        l = sum(a['Cyberloafer Type']==0)
-        h = sum(a['Cyberloafer Type']==1)
+        a = pd.DataFrame({'Cyberloafer Type': km.labels_,
+                          'Name of the Employee': df['name']})
+        l = sum(a['Cyberloafer Type'] == 0)
+        h = sum(a['Cyberloafer Type'] == 1)
         d = []
-        for i in range(0,len(a)):
-            dic = { 'name' : a.iloc[i][1],
-                            'prediction' : a.iloc[i][0].astype('str') }
+        for i in range(0, len(a)):
+            dic = {'name': a.iloc[i][1],
+                   'prediction': a.iloc[i][0].astype('str')}
             d.append(dic)
         res = {
-            'list' : d,
-            'graph' : {
-                'low' : l,
-                'high' : h
+            'list': d,
+            'graph': {
+                'low': l,
+                'high': h
             }
         }
         global model2FileData
         model2FileData = json.dumps(res)
 
-        a=pd.DataFrame({'Cyberloafer Type':km.labels_,'Name of the Employee':df['name']})
+        a = pd.DataFrame({'Cyberloafer Type': km.labels_,
+                          'Name of the Employee': df['name']})
 
-        clus['Cyberloafer Type']=a['Cyberloafer Type']
+        clus['Cyberloafer Type'] = a['Cyberloafer Type']
 
-        a.set_index('Name of the Employee',inplace=True)
-        X=clus[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable']]
-        y=clus['Cyberloafer Type']
+        a.set_index('Name of the Employee', inplace=True)
+        X = clus[['timeSpentOnInternet',
+                  'peopleAroundUsesInternet', 'internetUseEnjoyable']]
+        y = clus['Cyberloafer Type']
         global knn
         knn = KNeighborsClassifier()
-        knn.fit(X,y)
+        knn.fit(X, y)
         return jsonify({'message': "Model Trained"})
         # joblib.dump(knn, 'model3.pkl')
         # print("Model3 dumped!")
-    else :
+    else:
         return jsonify({'message': "No data here to train"})
 
 
@@ -92,39 +99,44 @@ def prediction():
         try:
             url = "https://falcons-cyber.firebaseio.com/train.json"
             m = requests.get(url)
-            n = m.json()   
-            if n: 
-                trans=pd.DataFrame(n)
-                df1=trans.T
+            n = m.json()
+            if n:
+                trans = pd.DataFrame(n)
+                df1 = trans.T
                 df1.set_index('employeeId', inplace=True)
-                X_pred=df1[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable','name']]
-                X1=X_pred[['timeSpentOnInternet','peopleAroundUsesInternet','internetUseEnjoyable']]
+                X_pred = df1[['timeSpentOnInternet',
+                              'peopleAroundUsesInternet', 'internetUseEnjoyable', 'name']]
+                X1 = X_pred[['timeSpentOnInternet',
+                             'peopleAroundUsesInternet', 'internetUseEnjoyable']]
                 knn_pred = knn.predict(X1)
-                out=pd.DataFrame({'cyberloaferType':knn_pred,'name':X_pred['name']})
-                out_1=out[['cyberloaferType']]
+                out = pd.DataFrame(
+                    {'cyberloaferType': knn_pred, 'name': X_pred['name']})
+                out_1 = out[['cyberloaferType']]
                 out.reset_index(inplace=True)
-                l = sum(out['cyberloaferType']==0)
-                h = sum(out['cyberloaferType']==1)
-                g=out.to_dict('records')
-                out.set_index('employeeId',inplace=True)
-                df4=df1[['productionScore']]
-                prod=pd.concat([out_1,df4],axis=1)
-                prod.dropna(how='any',subset=['productionScore'],axis=0,inplace=True)
-                prod['productionScore']=prod['productionScore'].astype(int)
+                l = sum(out['cyberloaferType'] == 0)
+                h = sum(out['cyberloaferType'] == 1)
+                g = out.to_dict('records')
+                out.set_index('employeeId', inplace=True)
+                df4 = df1[['productionScore']]
+                prod = pd.concat([out_1, df4], axis=1)
+                prod.dropna(how='any', subset=[
+                            'productionScore'], axis=0, inplace=True)
+                prod['productionScore'] = prod['productionScore'].astype(int)
                 prod.reset_index(inplace=True)
-                means = prod.groupby('cyberloaferType')['productionScore'].mean()
-                avgl=means[1]
-                avgh=means[0]
+                means = prod.groupby('cyberloaferType')[
+                    'productionScore'].mean()
+                avgl = means[1]
+                avgh = means[0]
                 res2 = {
-                    'list' : g,
-                    'graph1' : {
-                        'low' : l,
-                        'high' : h
-                    },
-                    'graph2' : {
-                        'low' : avgl,
-                        'high' : avgh
-                    }
+                    'list': g,
+                    'graph1': [
+                        {'x': 'low', 'y': l},
+                        {'x': 'high', 'y': h}
+                    ],
+                    'graph2': [
+                        {'x': 'low', 'y': avgl},
+                        {'x': 'high', 'y': avgh}
+                    ]
                 }
                 return json.dumps({'prediction': res2})
             else:
@@ -132,7 +144,7 @@ def prediction():
         except:
             return jsonify({'trace': traceback.format_exc()})
     else:
-        return jsonify({'message':'Train the model first'})
+        return jsonify({'message': 'Train the model first'})
 
 
 @app.route('/canalysis', methods=['GET'])
@@ -147,4 +159,4 @@ def canalysis():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
